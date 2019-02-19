83524b78e3b0f0c8c080503e13f5e953ffcf48c8
Burmaxwell <burluka@online.ua>
2019-02-19 19:49:07 +0200

    File

--- Entropy/Program.cs
+++ Entropy/Program.cs
@@ -3,13 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Entropy
 {
     class Program
     {
+
+
         static void Main(string[] args)
         {
+            string EncryptedFile = @"C:\Users\burlu\source\repos\Entropy\Encrypted.txt";
+            try
+            {
+                Console.WriteLine("******считываем весь файл********");//
+                using (StreamReader sr = new StreamReader(EncryptedFile))
+                {
+                    Console.WriteLine(sr.ReadToEnd());
+                }
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadKey();
         }
     }
 }
